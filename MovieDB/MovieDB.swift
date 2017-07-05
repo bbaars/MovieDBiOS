2f65ea82c9a0c48ecfa17b7d94cc6a5cca68ebeb
Brandon Baars <brandonbaars@gmail.com>
2017-07-05 19:16:07 -0400

    Added a simple API Request

--- MovieDB/MovieDB.swift
+++ MovieDB/MovieDB.swift
@@ -9,18 +9,46 @@
 import Foundation
 import Alamofire
 
+let APIKey = "6615c9824f812a6fb9b8b4ea5f49a285"
+let APIUrlPrefix = "https://api.themoviedb.org/3"
+let imageURLPrefix = "https://image.tmdb.org/t/p"
+
+/*  Example Request
+ https://api.themoviedb.org/3/discover/movie?with_people=287,819&sort_by=vote_average.desc&api_key=6615c9824f812a6fb9b8b4ea5f49a285
+ */
+
 
 class MovieDB {
     
-    private var _url: String
+    private var _url: String!
+    
+    fileprivate var parameters: Dictionary<String, AnyObject> = [
+        "api_key": APIKey as AnyObject,
+        "sort_by": "popularity.desc" as AnyObject
+    ]
     
     init(url: String) {
-        self._url = url;
+        _url = url;
     }
     
+    
+    /* obtains the JSON Request for the provided URL */
     func downloadMovieDBDetails(completed: @escaping DownloadComplete) {
         
         /* alamo fire request */
+        Alamofire.request("\(APIUrlPrefix)/discover/movie?with_people=287,819&sort_by=vote_average.desc", parameters: ["api_key": APIKey]).responseJSON { response in
+            
+            print(response)
+            
+            if let dict = response.result.value as? [String:AnyObject] {
+                
+                print(dict)
+                
+            }
+            
+            completed()
+        }
+        
     }
     
     
--- MovieDB/ViewController.swift
+++ MovieDB/ViewController.swift
@@ -10,10 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        var movie = MovieDB(url: "discover/movie?sort_by=popularity.desc")
+        movie.downloadMovieDBDetails {
+            
+            print("completed")
+        }
         
     }
 
